6ec8e462aa25610372e61d9fd2ff6be072701766
Fatih Muflih <102518888+fatihmuflih@users.noreply.github.com>
2024-06-04 01:00:25 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -52,7 +52,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*==================== typed js ====================*/
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'Youtuber', 'Blogger'],
+    strings: ['Frontend Developer', 'Backend Developer', 'Fullstack Developer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
